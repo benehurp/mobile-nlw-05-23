84626f691d1fdd64b160a7a627e99947a09f0ee1
Bene-Hur Pessoa <benehurp@gmail.com>
2023-05-20 22:28:33 -0300

    feat: day 3 nlw finished

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,10 @@
 import { StatusBar } from 'expo-status-bar'
 import { ImageBackground, Text, TouchableOpacity, View } from 'react-native'
-
+import * as SecureStore from 'expo-secure-store'
 import { makeRedirectUri, useAuthRequest } from 'expo-auth-session'
+import { styled } from 'nativewind'
+import { useEffect } from 'react'
+import { useRouter } from 'expo-router'
 
 import {
   useFonts,
@@ -10,22 +13,22 @@ import {
 } from '@expo-google-fonts/roboto'
 import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
 
-import blurBG from './src/assets/bg-blur.png'
-import NLWLogo from './src/assets/nlw-spacetime-logo.svg'
-import Stripes from './src/assets/stripes.svg'
-import { styled } from 'nativewind'
-import { useEffect } from 'react'
-import { api } from './src/lib/api'
+import blurBG from '../src/assets/bg-blur.png'
+import NLWLogo from '../src/assets/nlw-spacetime-logo.svg'
+import Stripes from '../src/assets/stripes.svg'
+import { api } from '../src/lib/api'
 
 const StyledStripes = styled(Stripes)
 
+const discovery = {
+  authorizationEndpoint: 'https://github.com/login/oauth/authorize',
+  tokenEndpoint: 'https://github.com/login/oauth/access_token',
+  revocationEndpoint:
+    'https://github.com/settings/connections/applications/1b9ca9695b32fa037d69',
+}
+
 export default function App() {
-  const discovery = {
-    authorizationEndpoint: 'https://github.com/login/oauth/authorize',
-    tokenEndpoint: 'https://github.com/login/oauth/access_token',
-    revocationEndpoint:
-      'https://github.com/settings/connections/applications/1b9ca9695b32fa037d69',
-  }
+  const router = useRouter()
 
   const [hasLoadedFonts] = useFonts({
     Roboto_400Regular,
@@ -44,17 +47,22 @@ export default function App() {
     discovery,
   )
 
+  async function handleGithubOAuthCode(code: string) {
+    const response = await api.post('/register', {
+      code,
+    })
+
+    const { token } = response.data
+
+    SecureStore.setItemAsync('token', token)
+
+    router.push('/memories')
+  }
+
   useEffect(() => {
     if (response?.type === 'success') {
       const { code } = response.params
-
-      api
-        .post('/register', {
-          code,
-        })
-        .then((response) => {
-          const { token } = response.data
-        })
+      handleGithubOAuthCode(code)
     }
   }, [response])
 
